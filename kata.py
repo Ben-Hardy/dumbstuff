3ef518d1ddf94d1a640b4c3e96f2d1b17cc2631c
Ben-Hardy <hardybe92@gmail.com>
2015-07-23 21:31:47 -0600

    Newer kata stuff
    Included is a caesar cipher and a hamming length calculator.

--- kata.py
+++ kata.py
@@ -1,5 +1,7 @@
 import math
 
+import unittest
+
 def getWinner(listOfBallots):
   #your code
   candidates = set(listOfBallots)
@@ -114,12 +116,85 @@ def pattern2(n):
 def f(n, m):
   return sum([i % m for i in xrange(1, n+1)])
 
+def pig_it(text):
+    words = text.split(" ")
+    
+    res = []
+    for word in words:
+        if word.isalpha():
+            tmp = word[1:] + word[0] + "ay"
+            res.append(tmp)
+        else: res.append(word)
+    return " ".join(res)
+
+def pig_it2(text):
+    return " ".join(i[1:] + i[0] + "ay" if i.isalpha() else i for i in text.split(" "))
+
+
+def hamming(a, b):
+    return len(a) - sum([a[i] == b[i] for i in range(len(a))])
+    
+    
+def triangle_number_generator(n):
+    a = 0
+    l = []
+    for i in range(1, n):
+        a += i
+        l.append(a)
+    return l
+
+# Caesar Cipher. SO UGLY
+# This is by far the ugliest solution I've come up with for a kata. UUGGHHHHHHH
+def encryptor(key, message):
+
+    # Chop off everything else past 26 so we only have numbers < 26. Gets rid of cases where key is > 26
+    if key > 0:
+        key = key % 26
+    else:
+        key = (abs(key) % 26) * -1 # gotta handle modulo for negatives too
+
+    if len(message) == 0:
+        return ""
+    else:
+        codes = [ord(i) for i in message]
+        res = []
+        for i in codes:
+            if i < 65 or i > 122 or (i > 90 and i < 97):
+                res.append(i)
+            elif i > 96:
+                if i + key > 122:
+                    res.append(i + key - 26)
+                elif i + key < 97:
+                    res.append(i + key + 26)
+                else: res.append(i + key)
+            elif i >= 65 and i < 91:
+                if i + key > 90:
+                    res.append(i + key - 26)
+                elif i + key < 65:
+                    res.append(i + key + 26)
+                else: res.append(i + key)
+        
+        return "".join([chr(i) for i in res])
+
+
+
+def is_divisible(*nums):
+    return len(nums) == sum([nums[0] % i == 0 for i in nums])
+
 def main():
     #print pattern2(5)
     #for i in range(1,1000):
      #   if (math.factorial(i-1)+1) % i == 0:
       #      print i 
-    print f(37131802, 75843520)
+    #print f(37131802, 75843520)
+    
+    #print(pig_it2('Pig latin is cool') == 'igPay atinlay siay oolcay')
+    #print(pig_it('This is my string') == 'hisTay siay ymay tringsay')
+    #print hamming("I like turtles","I like turkeys") == 3
+    #print triangle_number_generator(10000).index(1001820)
+    #print triangle_number_generator(100000)[-5]
+    print is_divisible(3,3,4)
+
 
 if __name__ == "__main__":
     main()
